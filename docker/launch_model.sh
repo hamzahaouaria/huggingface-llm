64607373bf0a4973ef7208499b6b517e1d69bc49
Hamza Haouaria <hamzahaouaria@gmail.com>
2024-10-28 22:59:44 +0100

    Add configuration files and scripts for Docker and npm
    - Added .npmrc file with engine-strict=true
    - Added .env.ci file with MONGODB_URL configuration
    - Added launch_mongo.sh script for launching MongoDB container
    - Added pre-commit script for lint-staged with lint-stage-config.js configuration
    - Added highlight-js.css file with import for atom-one-dark style
    - Added ambient.d.ts file for declaring module "*.ttf"
    - Added postcss.config.js file with tailwindcss and autoprefixer plugins
    - Added publicSepToken.ts file with export of PUBLIC_SEP_TOKEN constant
    - Added sum.ts file with sum function
    - Added Timestamps interface in Timestamps.ts file
    - Added isAborted store in isAborted.ts file
    - Added GET function in +server.ts file for healthcheck route
    - Added .dockerignore file with ignore patterns for Docker
    - Added isUrl.ts file with isURL function
    - Added ReviewStatus enum in Review.ts file
    - Added Chart.yaml file for chart configuration
    - Added Semaphore interface in Semaphore.ts file
    - Added lint-stage-config.js file for lint-staged configuration
    - Added .eslintignore file with ignore patterns for ESLint
    - Added isHuggingChat.ts file with isHuggingChat function
    - Added .prettierignore file with ignore patterns for Prettier
    - Added UrlDependency enum in UrlDependency.ts file
    - Added ConvSidebar interface in ConvSidebar.ts file
    - Added .gitignore file with ignore patterns for Git
    - Added models.ts file with findCurrentModel function
    - Added launch.json file for VS Code launch configuration
    - Added pendingMessage store in pendingMessage.ts file
    - Added TitleUpdate interface and writable in titleUpdate.ts file
    - Added MigrationResult interface in MigrationResult.ts file
    - Added isMessageId function in isMessageId.ts file
    - Added SerializedHTMLElement interface in types.ts file
    - Added TokenCache interface in TokenCache.ts file
    - Added .prettierrc file with Prettier configuration
    - Added logo.svg file in static/chatui directory
    - Added isDesktop function in isDesktop.ts file
    - Added deepestChild function in deepestChild.ts file

---
+++ docker/launch_model.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+model=HuggingFaceH4/zephyr-7b-beta
+# share a volume with the Docker container to avoid downloading weights every run
+volume=$PWD/data
+
+docker run --gpus all --shm-size 1g -p 8182:80 -v $volume:/data \
+    ghcr.io/huggingface/text-generation-inference:2.4.0 --model-id $model
